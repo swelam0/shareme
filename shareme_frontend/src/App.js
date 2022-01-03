7022794ffde6b866d6aa99d01a8df6692a4ac9c1
m-swelam <mid2o0o11@gmail.com>
2022-01-03 16:22:57 +0200

    react first commit

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center m-5">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
