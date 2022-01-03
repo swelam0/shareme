45ce34a3d6eb28f456d9cb114a00b697980a245e
m-swelam <mid2o0o11@gmail.com>
2022-01-03 16:38:58 +0200

    get project ready to start

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,7 +1,9 @@
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-center m-5">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-center m-5">
+        Hello world!
+      </h1>
     </div>
   )
 }
